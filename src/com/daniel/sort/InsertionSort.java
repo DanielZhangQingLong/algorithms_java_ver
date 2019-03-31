4444637b28db472b5162018412a3950a68046754
daniel <thecndra@gmail.com>
2019-03-31 19:35:12 +0800

    Add insertion sort

--- src/com/daniel/sort/InsertionSort.java
+++ src/com/daniel/sort/InsertionSort.java
@@ -1,8 +1,16 @@
 package com.daniel.sort;
 
-public class Example {
+public class InsertionSort {
 
-   public static void sort(Comparable[] a) { }
+   public static void sort(Comparable[] a) {
+      int N = a.length;
+      for (int i = 1; i < N; i++) {
+         for (int j = i; j > 0 && less(a[j], a[j-1]); j -- ) {
+//            if (less(a[j], a[j-1]))
+            exch(a, j, j-1);
+         }
+      }
+   }
 
    private static boolean less(Comparable v, Comparable w) {
       return v.compareTo(w) < 0;
