6490762f011ffa92741cfb17333d6215eaa6f701
daniel <thecndra@gmail.com>
2019-06-06 09:10:05 +0800

    Add merger sort, not tested yet

---
+++ src/com/daniel/sort/MergeSort.java
@@ -0,0 +1,24 @@
+package com.daniel.sort;
+
+public class MergeSort {
+
+    private static Comparable[] aux;
+
+    public static void merge(Comparable[] a, int lo, int mid, int hi) {
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        for (int k = lo; k <= hi; k ++) {
+            if (i > mid)                    a[k] = aux[j++];
+            else if (j > hi)                a[k] = aux[i++];
+            else if (less(aux[j], aux[i]))  a[k] = aux[j++];
+            else                            a[k] = aux[i++];
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+}
